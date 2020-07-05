4d903138e4fb2ae83fbe1c84f0042fcd1f328c36
Max56132 <50423072+MaxymGorn@users.noreply.github.com>
2020-07-05 03:04:25 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp31/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using WindowsFormsApp31;
+
+static class Program
+{
+    static bool IsSingleInstance()
+    {
+        try
+        {
+            Mutex.OpenExisting("UNIQUE");
+        }
+        catch
+        {
+            Mutex mutex = new Mutex(true, "UNIQUE");
+            return true;
+        }
+        MessageBox.Show("Приложение уже запущено!!!", "Дубликат!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        return false;
+    }
+
+
+    /// <summary>
+    /// Главная точка входа для приложения.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        if (IsSingleInstance())
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
